495a2ba1434046641c07df1aa21a18ceac3bf72c
Anton Korniychuk <dev@korniychuk.pro>
2019-08-19 11:11:31 +0300

    TypeORM configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,23 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { CatsService } from './cats/cats.service';
 import { CarsModule } from './cars/cars.module';
-import { APP_FILTER } from '@nestjs/core';
 import { TestFilter } from './test.filter';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CarsModule, AuthModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    CarsModule,
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController, CatsController],
   providers: [
     AppService,
